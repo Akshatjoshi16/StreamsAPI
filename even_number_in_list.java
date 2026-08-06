6c3ab561a04d94f0a01c3aa1f8d21edac5a31273
Akshatjoshi16 <akshatjoshii16@gmail.com>
2026-08-06 14:48:55 +0530

    Initial commit

---
+++ even_number_in_list.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class even_number_in_list {
+    public static void main(String[] args){
+        List<Integer> list=Arrays.asList(1,2,3,4,5,6,7,8,9);
+        List<Integer> ans=list.stream().filter(n->n%2==0).toList();
+        System.out.println(ans);
+    }
+
+}
