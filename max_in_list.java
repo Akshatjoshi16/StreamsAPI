1ca6bf482375bc1a7fe96cc6530dc6fd28787072
Akshatjoshi16 <akshatjoshii16@gmail.com>
2026-08-06 15:37:18 +0530

    Added finding max in list

---
+++ max_in_list.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class max_in_list {
+    public static void main(String[] args){
+        List<Integer> list= Arrays.asList(1,2,3,4,5,6);
+        int max=list.stream()
+                .max(Integer::compare)
+                .orElseThrow();
+        System.out.println(max);
+    }
+}
